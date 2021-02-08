6af4101c5ff5f4072698a77de85ae39afcabe4b8
Spandan Saxena <spandansaxena@gmail.com>
2021-02-08 20:25:57 +0530

    Renamed Main Activity to Splash Screen

--- app/src/main/java/com/aaxena/pragya/SplashScreen.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreen.java
@@ -1,14 +1,14 @@
 package com.aaxena.pragya;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.splash_screen);
     }
 }
