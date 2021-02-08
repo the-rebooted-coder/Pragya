db62779eafb2440c2d2a613c1cdf702bc31e09a5
Spandan Saxena <spandansaxena@gmail.com>
2021-02-08 20:59:34 +0530

    Replaced Lottie to Video

--- app/src/main/java/com/aaxena/pragya/SplashScreen.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.aaxena.pragya;
 
+import android.net.Uri;
 import android.os.Bundle;
+import android.widget.VideoView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +12,11 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+
+        VideoView videoview = findViewById(R.id.videoView);
+        Uri uri = Uri.parse("android.resource://"+getPackageName()+"/"+R.raw.splash_long);
+        videoview.setVideoURI(uri);
+        videoview.start();
     }
+
 }
