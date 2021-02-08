371898daa63450991b06005e72624731061ae083
Spandan Saxena <spandansaxena@gmail.com>
2021-02-08 21:24:38 +0530

    Added auto volume increment

--- app/src/main/java/com/aaxena/pragya/SplashScreen.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreen.java
@@ -1,6 +1,8 @@
 package com.aaxena.pragya;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
@@ -15,9 +17,18 @@ public class SplashScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.splash_screen);
+        setAudio();
         playVideo();
         playAudio();
 
+    }
+
+    /*
+    Sets Media Volume up so that if the user's volume is low the audio still sounds
+     */
+    private void setAudio() {
+        AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 10, 0);
     }
 
     private void playAudio() {
