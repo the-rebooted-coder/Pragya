47a7c27ffdda0ec21ebb1a6b14dd4104752dd3bb
Spandan Saxena <spandansaxena@gmail.com>
2021-02-09 14:29:30 +0530

    Intented Back to Sign Up

--- app/src/main/java/com/aaxena/pragya/SplashScreenLong.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreenLong.java
@@ -45,7 +45,7 @@ public class SplashScreenLong extends AppCompatActivity {
                     v2.vibrate(25);
                 }
                 //Intent i = new Intent(SplashScreenLong.this, SignUp.class);
-                Intent i = new Intent(SplashScreenLong.this, SplashScreenShort.class);
+                Intent i = new Intent(SplashScreenLong.this, SignUp.class);
                 startActivity(i);
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
