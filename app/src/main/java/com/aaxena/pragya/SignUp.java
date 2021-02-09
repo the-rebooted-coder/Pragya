30a4cd179bc96386a84c80c9900fde8e0707101a
Spandan Saxena <spandansaxena@gmail.com>
2021-02-09 14:52:42 +0530

    Added Google Sign In Button

--- app/src/main/java/com/aaxena/pragya/SignUp.java
+++ app/src/main/java/com/aaxena/pragya/SignUp.java
@@ -3,6 +3,7 @@ package com.aaxena.pragya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class SignUp extends AppCompatActivity {
 
