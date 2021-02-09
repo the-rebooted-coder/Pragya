21aed7cb9b673331062c696475bd9693b956a67d
Spandan Saxena <spandansaxena@gmail.com>
2021-02-09 20:12:16 +0530

    Added Another Sign in dependency

--- app/src/main/java/com/aaxena/pragya/SignUp.java
+++ app/src/main/java/com/aaxena/pragya/SignUp.java
@@ -5,7 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
 
+import com.google.android.gms.auth.api.signin.GoogleSignInClient;
+import com.google.firebase.auth.FirebaseAuth;
+
 public class SignUp extends AppCompatActivity {
+    private Button signInButton;
+    private GoogleSignInClient mGoogleSignInClient;
+    private String TAG = "Login";
+    private FirebaseAuth mAuth;
+    private int RC_SIGN_IN =1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
