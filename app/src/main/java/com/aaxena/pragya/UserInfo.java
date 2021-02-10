9c8214c1375adc7f25b7e95d08b111608fda3ede
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 17:14:04 +0530

    Added Vibration Control in UserInfo Acivity

--- app/src/main/java/com/aaxena/pragya/UserInfo.java
+++ app/src/main/java/com/aaxena/pragya/UserInfo.java
@@ -2,13 +2,82 @@ package com.aaxena.pragya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 public class UserInfo extends AppCompatActivity {
+    private static final String PREFS_NAME = "Vibration";
+    String TEXT = "text";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+    }
+    private void vibrateDevice() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String vibration_setting = sharedPreferences.getString(TEXT,"on");
+
+        if(vibration_setting.equals("on")) {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v3.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v3.vibrate(25);
+            }
+        }
+        else {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v3.vibrate(0);
+        }
+    }
+    @SuppressLint("ApplySharedPref")
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    SharedPreferences settings = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString(TEXT, "on");
+                    editor.commit();
+                    Toast.makeText(this,"Vibrations Turned On",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    vibrateDevice();
+                    int vib_delay = 100;
+                    new Handler().postDelayed(() -> {
+                        Vibrator v4 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            v4.vibrate(VibrationEffect.createOneShot(35, VibrationEffect.DEFAULT_AMPLITUDE));
+                        } else {
+                            //deprecated in API 26
+                            v4.vibrate(30);
+                        }
+                    }, vib_delay);
+                    SharedPreferences settings = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString(TEXT, "off");
+                    editor.commit();
+                    Toast.makeText(this,"Vibrations Turned Off",Toast.LENGTH_SHORT).show();
+
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
     }
 }
