3e92c0b307a406b5ea1950c8ecac207b04a3c024
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 14:33:43 +0530

    waiting on short splash for a while

--- app/src/main/java/com/aaxena/pragya/SplashScreenShort.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreenShort.java
@@ -1,10 +1,16 @@
 package com.aaxena.pragya;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Context;
 import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -24,24 +30,38 @@ public class SplashScreenShort extends AppCompatActivity {
         int randomIndex = new Random().nextInt(names.length);
         String randomName = names[randomIndex];
         textView.setText(randomName);
-        check();
+
+        int splash_screen_short = 2000;
+        new Handler().postDelayed(() -> {
+            final MediaPlayer mp = MediaPlayer.create(this, R.raw.short_click);
+            mp.start();
+            check();
+        }, splash_screen_short);
+
     }
 
     private void check() {
         GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
-        if (account !=null){
+        Intent i;
+        if (account != null) {
             //User Signed In, Proceeding to Landing
-            Intent i=new Intent(SplashScreenShort.this,Landing.class);
+            i = new Intent(SplashScreenShort.this, Landing.class);
             startActivity(i);
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+            finish();
+        } else {
+            //Newbie
+            i = new Intent(SplashScreenShort.this, SignUp.class);
+            startActivity(i);
             finish();
         }
-        else {
-            //Newbie
-            Intent i=new Intent(SplashScreenShort.this,SignUp.class);
-            startActivity(i);
-            overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-            finish();
+        startActivity(i);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        Vibrator v2 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            v2.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+        } else {
+            //deprecated in API 26
+            v2.vibrate(25);
         }
     }
 }
