ff859f5466b950f3b71f2f1564a149a3a342be75
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 13:16:04 +0530

    New retro red color to activities and volume vibration controls

--- app/src/main/java/com/aaxena/pragya/AboutDevs.java
+++ app/src/main/java/com/aaxena/pragya/AboutDevs.java
@@ -1,6 +1,15 @@
 package com.aaxena.pragya;
 
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
+import android.view.KeyEvent;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,12 +17,11 @@ import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
 public class AboutDevs extends AppCompatActivity {
-
+    private static final String PREFS_NAME = "Vibration";
+    String TEXT = "text";
     CarouselView carouselView;
 
     int[] sampleImages = {R.drawable.spandan,R.drawable.shrish,R.drawable.shubham,R.drawable.sriniv};
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,4 +32,65 @@ public class AboutDevs extends AppCompatActivity {
         carouselView.setImageListener(imageListener);
     }
     ImageListener imageListener = (position, imageView) -> imageView.setImageResource(sampleImages[position]);
+
+
+
+    private void vibrateDevice() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String vibration_setting = sharedPreferences.getString(TEXT,"on");
+
+        if(vibration_setting.equals("on")) {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v3.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v3.vibrate(25);
+            }
+        }
+        else {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v3.vibrate(0);
+        }
+    }
+    @SuppressLint("ApplySharedPref")
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        int action = event.getAction();
+        int keyCode = event.getKeyCode();
+        switch (keyCode) {
+            case KeyEvent.KEYCODE_VOLUME_UP:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    SharedPreferences settings = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString(TEXT, "on");
+                    editor.commit();
+                    Toast.makeText(this,"Vibrations Turned On",Toast.LENGTH_SHORT).show();
+                }
+                return true;
+            case KeyEvent.KEYCODE_VOLUME_DOWN:
+                if (action == KeyEvent.ACTION_DOWN) {
+                    vibrateDevice();
+                    int vib_delay = 100;
+                    new Handler().postDelayed(() -> {
+                        Vibrator v4 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+                        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                            v4.vibrate(VibrationEffect.createOneShot(35, VibrationEffect.DEFAULT_AMPLITUDE));
+                        } else {
+                            //deprecated in API 26
+                            v4.vibrate(30);
+                        }
+                    }, vib_delay);
+                    SharedPreferences settings = this.getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+                    SharedPreferences.Editor editor = settings.edit();
+                    editor.putString(TEXT, "off");
+                    editor.commit();
+                    Toast.makeText(this,"Vibrations Turned Off",Toast.LENGTH_SHORT).show();
+
+                }
+                return true;
+            default:
+                return super.dispatchKeyEvent(event);
+        }
+    }
 }
