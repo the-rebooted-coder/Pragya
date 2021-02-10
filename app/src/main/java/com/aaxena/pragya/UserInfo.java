c9791a2e899cbc56b83b9b0a8885fab5de245d6c
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 17:58:18 +0530

    Added User Info in BIOS Style

--- app/src/main/java/com/aaxena/pragya/UserInfo.java
+++ app/src/main/java/com/aaxena/pragya/UserInfo.java
@@ -1,7 +1,5 @@
 package com.aaxena.pragya;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
@@ -12,17 +10,90 @@ import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.KeyEvent;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.signin.GoogleSignIn;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
 
 public class UserInfo extends AppCompatActivity {
     private static final String PREFS_NAME = "Vibration";
     String TEXT = "text";
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_info);
+        TextView user_bios = findViewById(R.id.current_user);
+        user_bios.setText("->JUETPragya % ");
+        showInfoAsBios();
     }
+
+    @SuppressLint("SetTextI18n")
+    private void showInfoAsBios() {
+        GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
+        String personName = account.getDisplayName();
+        String personEmail = account.getEmail();
+
+        TextView user_bios = findViewById(R.id.current_user);
+
+        int splash_screen_short = 600;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % u_");
+        }, splash_screen_short);
+        int splash_screen_short2 = 1200;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % us_");
+        }, splash_screen_short2);
+        int splash_screen_short3 = 1800;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % use_");
+        }, splash_screen_short3);
+        int splash_screen_short4 = 2400;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user_");
+        }, splash_screen_short4);
+        int splash_screen_short5 = 3000;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n_");
+        }, splash_screen_short5);
+        int splash_screen_short6 = 3600;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====");
+        }, splash_screen_short6);
+        int splash_screen_short7 = 4200;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % e_");
+        }, splash_screen_short7);
+        int splash_screen_short8 = 4800;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % em_");
+        }, splash_screen_short8);
+        int splash_screen_short9 = 5400;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % ema_");
+        }, splash_screen_short9);
+        int splash_screen_short10 = 6000;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % emai_");
+        }, splash_screen_short10);
+        int splash_screen_short11 = 6600;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % email_");
+        }, splash_screen_short11);
+        int splash_screen_short12 = 7200;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % email\n\n====="+personEmail+"=====\n\n\n->JUETPragya % _");
+        }, splash_screen_short12);
+        int splash_screen_short13 = 7900;
+        new Handler().postDelayed(() -> {
+            user_bios.setText("->JUETPragya % user\n\n====="+personName+"=====\n\n->JUETPragya % email\n\n====="+personEmail+"=====\n\n\n->JUETPragya % Double Tap To Logout.");
+        }, splash_screen_short13);
+    }
+
     @Override
     public void onBackPressed() {
         super.onBackPressed();
