d70fee37acf43276eaba7cd6796ee707736193c3
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 11:59:16 +0530

    Added Menu Bar to Landing Activity

--- app/src/main/java/com/aaxena/pragya/Landing.java
+++ app/src/main/java/com/aaxena/pragya/Landing.java
@@ -1,8 +1,11 @@
 package com.aaxena.pragya;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 
 public class Landing extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class Landing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
+        setSupportActionBar(myToolbar);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
     }
 }
