82557e3f94f6cdc0cdcc70571f8d871a88c92236
Spandan Saxena <spandansaxena@gmail.com>
2021-02-10 18:33:12 +0530

    Update AboutPragya.java

--- app/src/main/java/com/aaxena/pragya/AboutPragya.java
+++ app/src/main/java/com/aaxena/pragya/AboutPragya.java
@@ -2,6 +2,7 @@ package com.aaxena.pragya;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class AboutPragya extends AppCompatActivity {
@@ -10,5 +11,13 @@ public class AboutPragya extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_pragya);
+    }
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        Intent toLanding = new Intent(AboutPragya.this,Landing.class);
+        startActivity (toLanding);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        finish();
     }
 }
