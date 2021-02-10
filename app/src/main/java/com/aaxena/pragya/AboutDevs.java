2c061ba3824597451cb3facce969f6a50ad6b288
Shrish Sharma <shrish0608@gmail.com>
2021-02-10 12:04:22 +0530

    Implemented CarouselView
    About Devs Activity

--- app/src/main/java/com/aaxena/pragya/AboutDevs.java
+++ app/src/main/java/com/aaxena/pragya/AboutDevs.java
@@ -3,12 +3,31 @@ package com.aaxena.pragya;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
+
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
 
 public class AboutDevs extends AppCompatActivity {
+
+    CarouselView carouselView;
+
+    int[] sampleImages = {};
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_devs);
+
+        carouselView = findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
     }
+    ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            imageView.setImageResource(sampleImages[position]);
+        }
+    };
 }
