0bdcde1c0bb4c28cf463450f004cd03bc988ec19
Spandan Saxena <spandansaxena@gmail.com>
2021-02-13 22:02:56 +0530

    Removed Dependency

--- app/src/main/java/com/aaxena/pragya/AboutPragya.java
+++ app/src/main/java/com/aaxena/pragya/AboutPragya.java
@@ -11,8 +11,9 @@ public class AboutPragya extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_pragya);
-    }
 
+    }
+    
     @Override
     public void onBackPressed() {
         super.onBackPressed();
