218f238c3952eeaa2e555c743ffef848f52c2473
Spandan Saxena <spandansaxena@gmail.com>
2021-02-16 20:06:03 +0530

    Fixed Google Sign in Intent to Landing and Added Retro Arrows

--- app/src/main/java/com/aaxena/pragya/SignUp.java
+++ app/src/main/java/com/aaxena/pragya/SignUp.java
@@ -27,6 +27,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.GoogleAuthProvider;
 
+import org.tensorflow.lite.examples.classification.ClassifierActivity;
+
 public class SignUp extends AppCompatActivity {
     private GoogleSignInClient mGoogleSignInClient;
     private String TAG = "Login";
@@ -90,7 +92,7 @@ public class SignUp extends AppCompatActivity {
             GoogleSignInAccount acc = completedTask.getResult(ApiException.class);
             GoogleSignInAccount account = GoogleSignIn.getLastSignedInAccount(getApplicationContext());
             if (account !=null){
-                Intent i=new Intent(SignUp.this,Landing.class);
+                Intent i=new Intent(SignUp.this, ClassifierActivity.class);
                 startActivity(i);
                 overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
                 finish();
