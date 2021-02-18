5854f25e5535f475cf00917d31ec28a5fbc855ff
Spandan Saxena <spandansaxena@gmail.com>
2021-02-18 23:36:27 +0530

    Added Audio in Short Splash and Vibs back

--- app/src/main/java/com/aaxena/pragya/SplashScreenShort.java
+++ app/src/main/java/com/aaxena/pragya/SplashScreenShort.java
@@ -1,13 +1,17 @@
 package com.aaxena.pragya;
 
+import android.content.Context;
 import android.content.Intent;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
+import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
+import android.os.VibrationEffect;
+import android.os.Vibrator;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 
 import java.util.Random;
 
@@ -29,11 +33,29 @@ public class SplashScreenShort extends AppCompatActivity {
         new Handler().postDelayed(() -> {
             final MediaPlayer mp = MediaPlayer.create(this, R.raw.short_click);
             mp.start();
+            vibrateDevice();
+            setAudio();
             Intent toLanding = new Intent(this, Landing.class);
             startActivity(toLanding);
             overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
             finish();
         }, splash_screen_short);
 
+    }
+    private void setAudio() {
+         /*
+        Sets Media Volume up so that if the user's volume is low the audio still sounds
+         */
+        AudioManager audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+        audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, 10, 0);
+    }
+    private void vibrateDevice() {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v3.vibrate(VibrationEffect.createOneShot(28, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v3.vibrate(25);
+            }
     }
 }
