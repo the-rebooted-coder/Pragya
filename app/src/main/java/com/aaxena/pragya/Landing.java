2af42775a7988d34846aea86935cf1740f6e80a0
Spandan Saxena <spandansaxena@gmail.com>
2021-02-18 21:25:59 +0530

    removed a whitespace

--- app/src/main/java/com/aaxena/pragya/Landing.java
+++ app/src/main/java/com/aaxena/pragya/Landing.java
@@ -24,7 +24,6 @@ public class Landing extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_landing);
-
         Toolbar myToolbar = (Toolbar) findViewById(R.id.my_toolbar);
         setSupportActionBar(myToolbar);
     }
