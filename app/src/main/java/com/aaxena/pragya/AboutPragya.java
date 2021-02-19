1ce6d118e672f1b581f8d88171bb488c78f163bc
Spandan Saxena <spandansaxena@gmail.com>
2021-02-20 00:43:43 +0530

    Added PlaceHolder Button and a Scroll View to About Pragya

--- app/src/main/java/com/aaxena/pragya/AboutPragya.java
+++ app/src/main/java/com/aaxena/pragya/AboutPragya.java
@@ -1,30 +1,79 @@
 package com.aaxena.pragya;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.VibrationEffect;
 import android.os.Vibrator;
 import android.view.KeyEvent;
+import android.widget.Button;
 import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import org.tensorflow.lite.examples.classification.ClassifierActivity;
 
 public class AboutPragya extends AppCompatActivity {
     private static final String PREFS_NAME = "Vibration";
     String TEXT = "text";
+    private final int STORAGE_PERMISSION_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_pragya);
+        downloadDocument();
 
+    }
+
+    private void downloadDocument() {
+        Button downloadDocumentation = findViewById(R.id.downloadDocument);
+        downloadDocumentation.setOnClickListener(v -> {
+            vibrateDevice();
+            int button_vib = 100;
+            new Handler().postDelayed(this::vibrateDeviceLightly, button_vib);
+            if (ContextCompat.checkSelfPermission(AboutPragya.this, Manifest.permission.WRITE_EXTERNAL_STORAGE)== PackageManager.PERMISSION_GRANTED)
+            {
+                }
+            else {
+                {
+                    new AlertDialog.Builder(AboutPragya.this)
+                            .setTitle("Please Grant Storage Permissions")
+                            .setMessage("To save the project file, pragya needs access to your internal storage")
+                            .setPositiveButton("Grant", new DialogInterface.OnClickListener() {
+                                @Override
+                                public void onClick(DialogInterface dialog, int which) {
+                                    requestStoragePermission();
+                                    vibrateDeviceLightly();
+
+                                }
+                            })
+                            .create().show();
+                }
+            }
+        });
+    }
+    private void requestStoragePermission(){
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            new AlertDialog.Builder(this)
+                    .setTitle("Can't Save Project File")
+                    .setMessage("Pragya Project Couldn't be Saved, because the download permission was denied!\n\nAllow Permission to Continue")
+                    .setPositiveButton("Okay", (dialog, which) -> ActivityCompat.requestPermissions(AboutPragya.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE))
+                    .setNegativeButton("Nope", (dialog, which) -> dialog.dismiss())
+                    .create().show();
+        }else {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},STORAGE_PERMISSION_CODE);
+        }
     }
 
     private void vibrateDevice() {
@@ -92,6 +141,23 @@ public class AboutPragya extends AppCompatActivity {
         startActivity(toLanding);
         overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
         finish();
+    }
+    private void vibrateDeviceLightly() {
+        SharedPreferences sharedPreferences = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE);
+        String vibration_setting = sharedPreferences.getString(TEXT, "on");
+
+        if (vibration_setting.equals("on")) {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                v3.vibrate(VibrationEffect.createOneShot(25, VibrationEffect.DEFAULT_AMPLITUDE));
+            } else {
+                //deprecated in API 26
+                v3.vibrate(23);
+            }
+        } else {
+            Vibrator v3 = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+            v3.vibrate(0);
+        }
     }
 }
 
